8a7b9cdfcd95a00c5be89c1bcbbf048e9ab16019
Guru Charan <gururk2@gmail.com>
2023-11-15 13:23:23 +0530

    Initial commit

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,23 @@
+const String appName = 'Redcosmos';
+
+const String packageName = 'com.redcosmos.shop';
+const String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+const String iosPackage = 'com.redcosmos.shop';
+const String iosLink = 'your ios link here';
+const String appStoreId = '123456789';
+
+const String deepLinkUrlPrefix = 'https://redcosmos.page.link';
+const String deepLinkName = 'redcosmos.com';
+
+const int timeOut = 50;
+const int perPage = 10;
+
+//Token ExpireTime in minutes & issuer name
+const int tokenExpireTime = 5;
+const String issuerName = 'eshop';
+
+const String baseUrl = 'https://fairfieldwholesale.us/app/v1/api/';
+const String jwtKey = "65c9cd19cd138f19ddf2f6320c7a802ee936c548";
+
+const String defaultCountryCode = 'IN';
